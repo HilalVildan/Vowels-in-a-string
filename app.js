8d2b7a4fd8c3d324c1873b9f827f524dfae9a3c0
HilalVildan <hilalsarioglu96@gmail.com>
2022-12-13 15:29:28 +0100

    styled update

--- app.js
+++ app.js
@@ -3,28 +3,22 @@ const buton = document.querySelector("#button");
 const text = document.querySelector(".text");
 
 buton.onclick = () => {
-    if (!input.value) {
+  if (!input.value) {
+    alert("Lütfen bir seyler yaziniz.!");
+  }
 
-        alert("Lütfen bir seyler yaziniz.!")
-        
-    }
+  let vowels;
+  if (input.value.match(/[aeiou]/gi)) {
+    vowels = input.value.match(/[aeiou]/gi).length;
+  } else {
+    vowels = 0;
+  }
 
-    let vowels;
-    if (input.value.match(/[aeiou]/gi)) {
-        vowels = input.value.match(/[aeiou]/gi).length
-        
-    } else {
-        vowels = 0;
-        
-    }
-
-    if (vowels===0) {
-        text.innerHTML = `There isn't vowel in ${input.value}`;
-        input.value = "";
-        
-    } else {
-        text.innerHTML = `There are ${vowels} vowel in "${input.value}"`;
-        input.value = "";
-        
-    }
-}
+  if (vowels === 0) {
+    text.innerHTML = `There isn't vowel in ${input.value}`;
+    input.value = "";
+  } else {
+    text.innerHTML = `There are ${vowels} vowel in "${input.value}"`;
+    input.value = "";
+  }
+};
